6825d68547639f360cc1ffd15448c862626bbc93
FranciszekCieslik <cieslik.franek@gmail.com>
2024-05-12 23:24:47 +0200

    adding styles to localStorage

--- client/src/firestore.js
+++ client/src/firestore.js
@@ -1,16 +1,16 @@
 import app from '@/firebaseConfig'
-import {getDoc, getFirestore, doc, setDoc, deleteDoc} from "firebase/firestore"; 
+import { getDoc, getFirestore, doc, setDoc, deleteDoc } from "firebase/firestore";
 import { getAuth, deleteUser } from "firebase/auth";
 
 const db = getFirestore(app);
 
-export async function updateUserSizes(){
-var sizes = JSON.parse(localStorage.getItem('LookLoomUserSizes'));
-const userRef = doc(db, 'users', JSON.parse(localStorage.getItem('LookLoomUser')).uid);
-await setDoc(userRef, { sizes:  sizes}, { merge: true });
+export async function updateUserSizes() {
+    var sizes = JSON.parse(localStorage.getItem('LookLoomUserSizes'));
+    const userRef = doc(db, 'users', JSON.parse(localStorage.getItem('LookLoomUser')).uid);
+    await setDoc(userRef, { sizes: sizes }, { merge: true });
 }
 
-export async function deleteAppUser(){
+export async function deleteAppUser() {
     //remove user data 
     const userRef = doc(db, 'users', JSON.parse(localStorage.getItem('LookLoomUser')).uid);
     await deleteDoc(userRef);
@@ -19,21 +19,38 @@ export async function deleteAppUser(){
     const auth = getAuth();
     const user = auth.currentUser;
     await deleteUser(user).then(() => {
-  // User deleted.
-}).catch((error) => {
-    console.log(error)
-});
+        // User deleted.
+    }).catch((error) => {
+        console.log(error)
+    });
 }
 
-export async function getUserSizes(){
-const userRef = doc(db, 'users', JSON.parse(localStorage.getItem('LookLoomUser')).uid);
-const userSnap = await getDoc(userRef);
+export async function getUserSizes() {
+    const userRef = doc(db, 'users', JSON.parse(localStorage.getItem('LookLoomUser')).uid);
+    const userSnap = await getDoc(userRef);
 
-if(userSnap){
-    localStorage.setItem('LookLoomUserSizes', JSON.stringify(
-        userSnap.data().sizes
-    ))
-    console.log(userSnap.data().sizes)
-}
+    if (userSnap) {
+        localStorage.setItem('LookLoomUserSizes', JSON.stringify(
+            userSnap.data().sizes
+        ))
+        // console.log(userSnap.data().sizes)
+    }
 }
 
+export async function updateUserStyles() {
+    var styles = JSON.parse(localStorage.getItem('LookLoomUserStyles'));
+    const userRef = doc(db, 'users', JSON.parse(localStorage.getItem('LookLoomUser')).uid);
+    await setDoc(userRef, { styles: styles }, { merge: true });
+}
+
+export async function getUserStyles() {
+    const userRef = doc(db, 'users', JSON.parse(localStorage.getItem('LookLoomUser')).uid);
+    const userSnap = await getDoc(userRef);
+
+    if (userSnap) {
+        localStorage.setItem('LookLoomUserStyles', JSON.stringify(
+            userSnap.data().sizes
+        ))
+        // console.log(userSnap.data().styles)
+    }
+}
